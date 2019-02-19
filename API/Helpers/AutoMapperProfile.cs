23e130a250ab6e31869cb242f2d36a3d7ac20d6e
Pablo Bayarri <pbayarri@openfinance.es>
2019-02-19 18:37:11 +0100

    Autenticación JWT y logeo básico de todas las requests y los responses

---
+++ API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using API.Dtos;
+using API.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
---
+++ API/Helpers/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
