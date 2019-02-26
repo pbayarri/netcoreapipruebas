0b3c91ef5377576410d7327341bb5a9f63772a07
Pablo Bayarri <pbayarri@openfinance.es>
2019-02-26 16:58:59 +0100

    adaptado el autorizado a los cambios de Jorge

--- API/Helpers/FunctionalityHandler.cs
+++ API/Helpers/FunctionalityHandler.cs
@@ -13,9 +13,9 @@ namespace API.Helpers
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, FunctionalityRequirement requirement)
         {
             Claim functionalities = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
-            var funcionalitiesList = functionalities.Value.Split(";");
+            var funcionalitiesList = functionalities.Value.Split(Functionality.FuncionalitySeparator);
 
-            if (funcionalitiesList.Contains(requirement.FuncionalityName))
+            if (funcionalitiesList.Contains(requirement.FuncionalityName.Substring(FunctionalityRoleAuthorizedAttribute.POLICY_PREFIX.Length)))
                 context.Succeed(requirement);
 
             return Task.CompletedTask;
