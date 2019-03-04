544c9778f5008460e3adae4d238d6728fc1050eb
Pablo Bayarri <pbayarri@openfinance.es>
2019-03-04 13:44:06 +0100

    endpoint para la versión del back y endpoint para el heartbeat

---
+++ API/Controllers/HeartbeatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HeartbeatController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHeartbeat()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/Controllers/VersionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetVersion()
+        {
+            Assembly assembly = Assembly.Load("API");
+            var fields = assembly.GetName().Version.ToString().Split('.');
+            string versionInfo = $"{fields[0]}.{fields[1]}.{fields[2]}";
+            return Ok(versionInfo);
+        }
+    }
+}
