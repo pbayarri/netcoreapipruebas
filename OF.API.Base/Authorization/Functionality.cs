9a2d98201114d03193a4bb210c5b3fbf96f0b7e0
Pablo Bayarri <pbayarri@openfinance.es>
2019-03-12 18:20:15 +0100

    esto ya no hace falta

--- OF.API.Base/Authorization/Functionality.cs
+++ OF.API.Base/Authorization/Functionality.cs
@@ -10,25 +10,7 @@ namespace OF.API.Base.Authorization
     public static class Functionality
     {
         public const string FuncionalitySeparator = ";";
-        public enum Functionalities
-        {
-            GetAllUsers,
-            GetOneUser
-        }
-
-        public static System.Action<AuthorizationOptions> GetRoleFunctionalityOptions()
-        {
-            return options =>
-            {
-                foreach (var functionality in System.Enum.GetValues(typeof(Functionality.Functionalities)))
-                {
-                    string policyName = $"{FunctionalityRoleAuthorizedAttribute.POLICY_PREFIX}{functionality.ToString()}";
-                    options.AddPolicy(policyName, policy =>
-                        policy.Requirements.Add(new FunctionalityRequirement(policyName)));
-                }
-            };
-        }
-
+       
         public static void AddRoleFunctionalities(this IServiceCollection services)
         {
             services.AddSingleton<IAuthorizationRequirementBuilder, FunctionalityRequirementBuilder>();
